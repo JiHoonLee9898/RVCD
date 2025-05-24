ab0de46c76c1202b52e381123850e66a9b7451c6
JiHoonLee9898 <zailitol@naver.com>
2025-05-24 11:14:38 +0000

    20250524 chair_bleu

--- MAIN_CODES/test_codes/run_example_test.sh
+++ MAIN_CODES/test_codes/run_example_test.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 ### RVCD for CHAIR/BLEU ###
-CUDA_VISIBLE_DEVICES=0 python /home/work/jihoon_wombat_storage/RVCD/MAIN_CODES/test_codes/test.py \
-    --model llava-1.5 \
+CUDA_VISIBLE_DEVICES=0 python /home/work/jihoon_wombat_storage/RVCD/MAIN_CODES/test.py \
+    --model mplug-owl2 \
     --ref_folder_path /home/work/jihoon_wombat_storage/RVCD/DB_single_concept_images_flux_generated/generated_images \
     --data_path /home/work/jihoon_wombat_storage/COCO_DIR \
     --chair_cache_path /home/work/jihoon_wombat_storage/RVCD/MAIN_CODES/eval/CHAIR_CACHE/chair.pkl \
@@ -13,5 +13,4 @@ CUDA_VISIBLE_DEVICES=0 python /home/work/jihoon_wombat_storage/RVCD/MAIN_CODES/t
     --output_dir ./generated_captions/ \
     --rvcd_alpha 1 \
     --rvcd_beta 0 \
-    --kv_cache_faster False
-
+    --kv_cache_faster True
