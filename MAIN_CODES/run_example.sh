a5f7b75b170d392b696bb61cbbcce2fe1d7eed9d
JiHoonLee9898 <zailitol@naver.com>
2025-05-26 23:45:09 +0000

    run_example.sh update

--- MAIN_CODES/run_example.sh
+++ MAIN_CODES/run_example.sh
@@ -15,36 +15,6 @@ CUDA_VISIBLE_DEVICES=0 python /home/work/jihoon_wombat_storage/RVCD/MAIN_CODES/r
     --rvcd_beta 0.1 \
     --kv_cache_faster True
 
-CUDA_VISIBLE_DEVICES=0 python /home/work/jihoon_wombat_storage/RVCD/MAIN_CODES/rvcd_generation_chair_bleu.py \
-    --model minigpt4 \
-    --ref_folder_path /home/work/jihoon_wombat_storage/RVCD/DB_single_concept_images_flux_generated/generated_images \
-    --data_path /home/work/jihoon_wombat_storage/COCO_DIR \
-    --chair_cache_path /home/work/jihoon_wombat_storage/RVCD/MAIN_CODES/eval/CHAIR_CACHE/chair.pkl \
-    --yolo_version yolov8x.pt \
-    --num_samples 500 \
-    --seed 42 \
-    --gpu-id 0 \
-    --output_dir ./generated_captions/ \
-    --rvcd_alpha 1 \
-    --rvcd_beta 0.1 \
-    --kv_cache_faster True
-
-CUDA_VISIBLE_DEVICES=0 python /home/work/jihoon_wombat_storage/RVCD/MAIN_CODES/rvcd_generation_chair_bleu.py \
-    --model mplug-owl2 \
-    --ref_folder_path /home/work/jihoon_wombat_storage/RVCD/DB_single_concept_images_flux_generated/generated_images \
-    --data_path /home/work/jihoon_wombat_storage/COCO_DIR \
-    --chair_cache_path /home/work/jihoon_wombat_storage/RVCD/MAIN_CODES/eval/CHAIR_CACHE/chair.pkl \
-    --yolo_version yolov8x.pt \
-    --num_samples 500 \
-    --seed 42 \
-    --gpu-id 0 \
-    --output_dir ./generated_captions/ \
-    --rvcd_alpha 1 \
-    --rvcd_beta 0.1 \
-    --kv_cache_faster True
-
-
-
 # ### RVCD for POPE ###
 # CUDA_VISIBLE_DEVICES=0 python rvcd_generation_pope.py \
 #     --model llava-1.5 \
