c2cdc616d5dfcf57cd5bcbda571f60b5b0612579
JiHoonLee9898 <zailitol@naver.com>
2026-05-24 13:56:02 +0000

    eval

--- MAIN_CODES/run_example.sh
+++ MAIN_CODES/run_example.sh
@@ -1,32 +1,120 @@
 #!/bin/bash
 
+COMMON_DATA_PATH="/home/jihoon/jihoon/DATASETS/coco2014/val2014"
+OUT_DIR="./generated_captions/"
+NUM_SAMPLES=300
+SEED=42
+GPU_ID=0
+MAX_NEW_TOKENS=64
+CHAIR_CACHE_PATH="eval/CHAIR_CACHE/chair.pkl"
+
 ### RVCD for CHAIR/BLEU ###
-# CUDA_VISIBLE_DEVICES=0 python rvcd_generation_chair_bleu.py \
-#     --model llava-1.5 \
-#     --ref_folder_path DB_single_concept_images_flux_generated/generated_images \
-#     --data_path /home/jihoon/jihoon/DATASETS/coco2014/val2014 \
-#     --chair_cache_path eval/CHAIR_CACHE/chair.pkl \
-#     --yolo_version yolov8x.pt \
-#     --num_samples 100 \
-#     --seed 42 \
-#     --gpu-id 0 \
-#     --output_dir ./generated_captions/ \
-#     --rvcd_alpha 1 \
-#     --rvcd_beta 0.1 \
-#     --kv_cache_faster True
+CUDA_VISIBLE_DEVICES=${GPU_ID} python rvcd_generation_chair_bleu.py \
+    --model llava-1.5 \
+    --ref_folder_path DB_single_concept_images_flux_generated/generated_images \
+    --data_path ${COMMON_DATA_PATH} \
+    --chair_cache_path ${CHAIR_CACHE_PATH} \
+    --yolo_version yolov8x.pt \
+    --num_samples ${NUM_SAMPLES} \
+    --seed ${SEED} \
+    --gpu-id ${GPU_ID} \
+    --output_dir ${OUT_DIR} \
+    --max_new_tokens ${MAX_NEW_TOKENS} \
+    --rvcd_alpha 1 \
+    --rvcd_beta 0.1 \
+ 
 
-CUDA_VISIBLE_DEVICES=0 python prior_decodings/prior_generation_chair_bleu.py \
+
+### greedy for CHAIR/BLEU ###
+CUDA_VISIBLE_DEVICES=${GPU_ID} python prior_decodings/prior_generation_chair_bleu.py \
     --model not_rvcd_llava \
-    --data_path /home/jihoon/jihoon/DATASETS/coco2014/val2014 \
+    --data_path ${COMMON_DATA_PATH} \
     -d greedy \
-    --num_samples 100 \
-    --seed 43 \
-    --gpu-id 0 \
-    --output_dir ./generated_captions/ 
+    --num_samples ${NUM_SAMPLES} \
+    --seed ${SEED} \
+    --gpu-id ${GPU_ID} \
+    --output_dir ${OUT_DIR} \
+    --max_new_tokens ${MAX_NEW_TOKENS} \
+    --chair_cache_path ${CHAIR_CACHE_PATH}
 
 
+### dola for CHAIR/BLEU ###
+CUDA_VISIBLE_DEVICES=${GPU_ID} python prior_decodings/prior_generation_chair_bleu.py \
+    --model not_rvcd_llava \
+    --data_path ${COMMON_DATA_PATH} \
+    -d dola \
+    --num_samples ${NUM_SAMPLES} \
+    --seed ${SEED} \
+    --gpu-id ${GPU_ID} \
+    --output_dir ${OUT_DIR} \
+    --max_new_tokens ${MAX_NEW_TOKENS} \
+    --chair_cache_path ${CHAIR_CACHE_PATH}
 
 
+### halc for CHAIR/BLEU ###
+CUDA_VISIBLE_DEVICES=${GPU_ID} python prior_decodings/prior_generation_chair_bleu.py \
+    --model not_rvcd_llava \
+    --data_path ${COMMON_DATA_PATH} \
+    -d halc \
+    --num_samples ${NUM_SAMPLES} \
+    --seed ${SEED} \
+    --gpu-id ${GPU_ID} \
+    --output_dir ${OUT_DIR} \
+    --max_new_tokens ${MAX_NEW_TOKENS} \
+    --chair_cache_path ${CHAIR_CACHE_PATH} \
+    --beam 3 \
+    --k-candidate-num 4 \
+    --expand-ratio 0.6 \
+    --detector dino \
+    --box_threshold 0.4
+
+
+### opera for CHAIR/BLEU ###
+CUDA_VISIBLE_DEVICES=${GPU_ID} python prior_decodings/prior_generation_chair_bleu.py \
+    --model not_rvcd_llava \
+    --data_path ${COMMON_DATA_PATH} \
+    -d opera \
+    --num_samples ${NUM_SAMPLES} \
+    --seed ${SEED} \
+    --gpu-id ${GPU_ID} \
+    --output_dir ${OUT_DIR} \
+    --max_new_tokens ${MAX_NEW_TOKENS} \
+    --chair_cache_path ${CHAIR_CACHE_PATH} \
+    --beam 3 \
+    --scale_factor 50 \
+    --threshold 15 \
+    --num_attn_candidates 5 \
+    --penalty_weights 1.0
+
+
+### vcd for CHAIR/BLEU ###
+CUDA_VISIBLE_DEVICES=${GPU_ID} python prior_decodings/prior_generation_chair_bleu.py \
+    --model not_rvcd_llava \
+    --data_path ${COMMON_DATA_PATH} \
+    -d vcd \
+    --num_samples ${NUM_SAMPLES} \
+    --seed ${SEED} \
+    --gpu-id ${GPU_ID} \
+    --output_dir ${OUT_DIR} \
+    --max_new_tokens ${MAX_NEW_TOKENS} \
+    --chair_cache_path ${CHAIR_CACHE_PATH} \
+    --cd_alpha 1 \
+    --cd_beta 0.1 \
+    --noise_step 500
+
+
+### beam for CHAIR/BLEU ###
+CUDA_VISIBLE_DEVICES=${GPU_ID} python prior_decodings/prior_generation_chair_bleu.py \
+    --model not_rvcd_llava \
+    --data_path ${COMMON_DATA_PATH} \
+    -d beam \
+    --num_samples ${NUM_SAMPLES} \
+    --seed ${SEED} \
+    --gpu-id ${GPU_ID} \
+    --output_dir ${OUT_DIR} \
+    --max_new_tokens ${MAX_NEW_TOKENS} \
+    --chair_cache_path ${CHAIR_CACHE_PATH} \
+    --beam 3
 
 
 
